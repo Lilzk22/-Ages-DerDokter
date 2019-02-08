8efc13d4fdaf4c22354f4c53f664bcb03c405f84
Marcos <zombiekiller199914@gmail.com>
2019-02-07 18:11:58 -0600

    Interactive Script

---
+++ Assets/Scripts/Interfaces/IInteractive.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ /// <summary>
+ /// Interface for elements the player can interct with
+ /// </summary>
+
+public interface IInteractive 
+{
+    void InteractWith();
+   
+}
