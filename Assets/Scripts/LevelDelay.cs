a458c5df6013772cb1179583d830525d2f1093c8
Marcos <zombiekiller199914@gmail.com>
2019-03-29 16:04:57 -0500

    Scene Loading Error

--- Assets/Scripts/LevelDelay.cs
+++ Assets/Scripts/LevelDelay.cs
@@ -8,17 +8,14 @@ public class LevelDelay : MonoBehaviour
     [SerializeField]
     private float delayBeforeLoading = 10f;
 
-    [SerializeField]
-    private string sceneNameToLoad;
-
     private float timeElapsed;
 
     void Update()
     {
         timeElapsed = -Time.deltaTime;
-        if (timeElapsed>delayBeforeLoading)
+        if (timeElapsed > delayBeforeLoading)
         {
-            SceneManager.LoadScene(sceneNameToLoad);
+            SceneManager.LoadScene(3);
         }
     }
 }
