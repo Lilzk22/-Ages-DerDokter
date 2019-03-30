2ef9d578f6f5b61e6246875e8a26bdc3bff528ee
Marcos <zombiekiller199914@gmail.com>
2019-03-30 14:53:32 -0500

    DreamBox Done

---
+++ Assets/Scripts/AuioTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuioTrigger : MonoBehaviour
+{
+    public AudioClip SoundPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter()
+    {
+       if(!alreadyPlayed)
+       {
+            audio.PlayOneShot(SoundPlay, Volume);
+            alreadyPlayed = true;
+       }
+    }
+}
--- Assets/Scripts/LevelDelay.cs
+++ Assets/Scripts/LevelDelay.cs
@@ -1,21 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelDelay : MonoBehaviour
 {
     [SerializeField]
     private float delayBeforeLoading = 10f;
+    [SerializeField]
+    private string sceneNameToLoad;
 
     private float timeElapsed;
 
     void Update()
     {
-        timeElapsed = -Time.deltaTime;
+        timeElapsed += Time.deltaTime;
         if (timeElapsed > delayBeforeLoading)
         {
-            SceneManager.LoadScene(3);
+            SceneManager.LoadScene(sceneNameToLoad);
         }
     }
+
+
+    
 }
