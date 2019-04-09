a5ddbe9c6f79730c56b4c9bbac967c8d16e64cb7
Marcos <zombiekiller199914@gmail.com>
2019-04-09 12:04:59 -0500

    PowerGenerator start

---
+++ Assets/Scripts/RotatingObjects.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObjects : MonoBehaviour
+{
+    public GameObject gearOne;
+
+    void Update()
+    {
+        gearOne.transform.Rotate(0, 1, 0);
+    }
+}
