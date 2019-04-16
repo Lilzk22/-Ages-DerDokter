7c75ac82b69cfc5e8fdbcfde3fed22c9790ebdb7
Marcos <zombiekiller199914@gmail.com>
2019-04-16 14:46:26 -0500

    Pause Menu done
    Fix bug with cursor

---
+++ Assets/PauseScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PauseScript : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    private RigidbodyFirstPersonController rigidbodyFirstPersonController;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+   public void Resume()
+   {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        //Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+
+   }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        //Cursor.visible = true;
+        //Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
