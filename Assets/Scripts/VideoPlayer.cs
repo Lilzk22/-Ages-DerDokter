aae41d7ba3d73279c992ea66d112a80365acd224
Marcos <zombiekiller199914@gmail.com>
2019-05-05 19:03:33 -0500

    EndLevel BEgun

---
+++ Assets/Scripts/VideoPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoPlayer : MonoBehaviour
+{
+    public GameObject videoPlayer;
+    public int timeToStop;
+
+    void Start()
+    {
+        videoPlayer.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider Player)
+    {
+        if(Player.gameObject.tag=="Player")
+        {
+            videoPlayer.SetActive(true);
+            Destroy(videoPlayer, timeToStop);
+        }
+    }
+}
